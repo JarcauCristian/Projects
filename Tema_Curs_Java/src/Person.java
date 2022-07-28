283b5b7f57e5d41faf35b80d6aff00ae13504aaa
Stefan-Cristian Jarcau <stefan.jarcau@student.unitbv.ro>
2022-07-28 14:13:11 +0300

    Modified Person file

--- Tema_Curs_Java/src/Person.java
+++ Tema_Curs_Java/src/Person.java
@@ -1,23 +1,25 @@
 import java.util.Date;
 
 public class Person {
-    private Long registrationNumber;
+    private String CNP;
     private String firstName;
     private String lastName;
     private Date dateOfBirth;
     private String gender;
     private Integer yearOfDriverLicenseEmited;
 
-    public Person(Long registrationNumber, String firstName, String lastName, String gender, Integer yearOfDriverLicenseEmited) {
-        this.registrationNumber = registrationNumber;
-        this.firstName = firstName;
+    public Person(String CNP, String firstName, String lastName, String gender, Integer yearOfDriverLicenseEmited) {
+        if(CNP.length() == 13)
+        {
+            this.CNP = CNP;
+        }
         this.lastName = lastName;
         this.gender = gender;
         this.yearOfDriverLicenseEmited = yearOfDriverLicenseEmited;
     }
 
     public Person() {
-        this.registrationNumber = Long.valueOf(0);
+        this.CNP = "";
         this.dateOfBirth = new Date();
         this.firstName = "";
         this.lastName = "";
